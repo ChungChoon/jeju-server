8ba02d1b6c4e36f923d2f9dc5e00e03ffff41d9d
배지원 <bjiwon2005@naver.com>
2018-10-23 21:13:12 +0900

    server set

--- routes/user/sign_up.js
+++ routes/user/sign_up.js
@@ -19,6 +19,12 @@ router.post('/', async (req, res, next) => {
         hp,
         addr
     } = req.body;
+    // 기본 공통값 받고, Null 검사
+    // 농부계정인지 일반계정인지 구분
+    // 중복검사 쿼리수행
+    // 통과 여부에 따라 패스워드 생성
+    // 디비 저장 쿼리수행
+
     if (flag == 1) {
         console.log('농부 계정 가입');
     } else if (flag == 2) {
