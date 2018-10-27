d9c5225c0d63436ba47747abd949f9e653340547
배지원 <bjiwon2005@naver.com>
2018-10-27 19:52:18 +0900

    modify home

--- routes/home/home_router.js
+++ routes/home/home_router.js
@@ -5,14 +5,29 @@ const express = require('express'),
     db = require('../../module/db_transction'),
     jwt = require('../../module/jwt');
 
-// const sign_up = require('./sign_up');
+const filter_lecture = require('./filter_lecture');
 
-// router.use('/signup', sign_up);
+router.use('/filter', filter_lecture);
 
 router.get('/', async (req, res, next) => {
-    let popular_query = `select a.user_pk, a.mail, a.name, a.birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, b.* from farmer_info a, lecture b, lecture_owner c where a.user_pk = c.user_fk order by apply limit 6;`;
-    let offline_query = `select a.user_pk, a.mail, a.name, a.birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, b.* from farmer_info a, lecture b, lecture_owner c where a.user_pk = c.user_fk and b.kind in (3, 4, 5, 6, 7)`;
-    let online_query = `select a.user_pk, a.mail, a.name, a.birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, b.* from farmer_info a, lecture b, lecture_owner c where a.user_pk = c.user_fk and b.kind in (8, 9, 10, 11)`;
+    // let popular_query = `select a.user_pk, a.mail, a.name, a.birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, a.farmer_career, a.farm_name, a.reg_num, a.farm_pk, a.farm_addr, a.subject, a.kind, a.farm_img, b.* from farmer_info a, lecture b, lecture_owner c where a.user_pk = c.user_fk order by apply limit 6;`;
+    let popular_query = `
+        select a.user_pk, a.mail, a.name, date_format(a.birth, "%Y-%m-%d") as birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, a.farmer_career, a.farm_name, a.reg_num, a.farm_pk, a.farm_addr, a.subject, a.kind, a.farm_img, b.title, b.target, b.kind, b.period, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply 
+        from farmer_info a, lecture b, lecture_owner c 
+        where a.user_pk = c.user_fk  and b.lecture_pk = c.lecture_fk
+        order by apply 
+        limit 6
+        `;
+    let offline_query = `
+        select a.user_pk, a.mail, a.name, date_format(a.birth, "%Y-%m-%d") as birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, a.farmer_career, a.farm_name, a.reg_num, a.farm_pk, a.farm_addr, a.subject, a.kind, a.farm_img, b.title, b.target, b.kind, b.period, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply 
+        from farmer_info a, lecture b, lecture_owner c 
+        where a.user_pk = c.user_fk  and b.lecture_pk = c.lecture_fk and b.kind in (3, 4, 5, 6, 7)
+        `;
+    let online_query = `
+        select a.user_pk, a.mail, a.name, date_format(a.birth, "%Y-%m-%d") as birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, a.farmer_career, a.farm_name, a.reg_num, a.farm_pk, a.farm_addr, a.subject, a.kind, a.farm_img, b.title, b.target, b.kind, b.period, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply 
+        from farmer_info a, lecture b, lecture_owner c 
+        where a.user_pk = c.user_fk  and b.lecture_pk = c.lecture_fk and b.kind in (8, 9, 10, 11)
+        `;
     let popular_result = await db.queryParamNone(popular_query);
     let offline_result = await db.queryParamNone(offline_query);
     let online_result = await db.queryParamNone(online_query);
