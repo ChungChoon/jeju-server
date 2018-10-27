090cc70d25376b134855550d2ac88b199e0ce971
배지원 <bjiwon2005@naver.com>
2018-10-27 12:18:56 +0900

    add lecture all list

--- routes/lecture/lecture_router.js
+++ routes/lecture/lecture_router.js
@@ -1,15 +1,32 @@
 "use strict";
 
 const express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    db = require('../../module/db_transction');
 
 const lecture_create = require('./lecture_create');
 
 router.use('/create', lecture_create);
 
 router.get('/', async (req, res, next) => {
-    let select_query = `select * from lecuture`;
+    let select_query = `select a.user_pk, a.mail, a.name, a.birth, a.sex, a.hp, a.img, a.wallet_addr, a.user_gb, b.* from farmer_info a, lecture b, lecture_owner c where a.user_pk = c.user_fk and c.lecture_fk =b.lecture_pk`;
+    let select_result = await db.queryParamNone(select_query);
 
+    if (!select_result) {
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    } else if (select_result.length === 0) {
+        console.log(select_result[0]);
+        res.status(200).json({
+            message: "doesn's exist"
+        })
+    } else {
+        res.status(200).json({
+            message: "Success To Get Information",
+            data: select_result
+        })
+    }
 })
 
 module.exports = router;
