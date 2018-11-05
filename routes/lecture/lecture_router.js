2f0e5a1f936eadb0124d08090b580cc243591891
배지원 <bjiwon2005@naver.com>
2018-11-05 19:27:38 +0900

    add lecture detail

--- routes/lecture/lecture_router.js
+++ routes/lecture/lecture_router.js
@@ -72,10 +72,36 @@ router.get('/:id', async (req, res, next) => {
                     where lecture_pk = ?
                     `;
                 let select_result = await db.queryParamArr(select_query, [decoded.user_idx, decoded.user, req.params.id]);
-
+                if (!select_result) {
+                    res.status(500).json({
+                        message : "Internal Server Error"
+                    });
+                } else  {
+                    res.status(200).json({
+                        message : "Success Get Lecture Detail",
+                        data: select_result[0]
+                    });
+                }
             }
         } else {
-            let select_query = ``;
+            let select_query = `
+            select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+            from farmer_info a 
+            join lecture b 
+            on a.user_pk = b.owner_fk
+            where lecture_pk = ?
+            `;
+            let select_result = await db.queryParamArr(select_query, [req.params.id]);
+            if (!select_result) {
+                res.status(500).json({
+                    message : "Internal Server Error"
+                });
+            } else  {
+                res.status(200).json({
+                    message : "Success Get Lecture Detail",
+                    data: select_result[0]
+                });
+            }
         }
     }
 
