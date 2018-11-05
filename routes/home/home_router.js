1f99852846d07c484ead000ee7f91514afe54922
배지원 <bjiwon2005@naver.com>
2018-11-05 20:07:13 +0900

    add home

--- routes/home/home_router.js
+++ routes/home/home_router.js
@@ -27,26 +27,37 @@ router.get('/', async (req, res, next) => {
         }
         else {
             let popular_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk
-                        order by apply
+                        select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
+                        left outer join lecture_apply c
+                        on c.lecture_fk = b.lecture_pk and c.user_fk = ?
+                        order by apply desc
                         limit 6
                         `;
             let offline_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ? ) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk and b.kind in (3, 4, 5, 6, 7)
+                        select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
+                        left outer join lecture_apply c
+                        on c.lecture_fk = b.lecture_pk and c.user_fk = ?
+                        where b.kind in (3, 4, 5, 6, 7)
                         `;
             let online_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ? ) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk and b.kind in (8, 9, 10, 11)
+                        select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
+                        left outer join lecture_apply c
+                        on c.lecture_fk = b.lecture_pk and c.user_fk = ?
+                        where b.kind in (8, 9, 10, 11)
                         `;
 
-            let popular_result = await db.queryParamArr(popular_query, [decoded.user_idx]);
-            let offline_result = await db.queryParamArr(offline_query, [decoded.user_idx]);
-            let online_result = await db.queryParamArr(online_query, [decoded.user_idx]);
+            let popular_result = await db.queryParamArr(popular_query, [decoded.user_idx, decoded.user_idx]);
+            let offline_result = await db.queryParamArr(offline_query, [decoded.user_idx, decoded.user_idx]);
+            let online_result = await db.queryParamArr(online_query, [decoded.user_idx, decoded.user_idx]);
 
             if (!popular_result || !offline_result || !online_result) {
                 res.status(500).json({
@@ -64,21 +75,26 @@ router.get('/', async (req, res, next) => {
     }
     else {
         let popular_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
                         order by apply
                         limit 6
                         `;
         let offline_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ? ) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk and b.kind in (3, 4, 5, 6, 7)
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
+                        where b.kind in (3, 4, 5, 6, 7)
                         `;
         let online_query = `
-                        select(select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ? ) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a, lecture b
-                        where a.user_pk = b.owner_fk and b.kind in (8, 9, 10, 11)
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a 
+                        join lecture b 
+                        on a.user_pk = b.owner_fk
+                        where b.kind in (8, 9, 10, 11)
                         `;
 
         let popular_result = await db.queryParamArr(popular_query, [decoded.user_idx]);
