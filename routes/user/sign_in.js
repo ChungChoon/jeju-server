da02430ed33c7dbc460cdd097f12a97bd2acb2dc
배지원 <bjiwon2005@naver.com>
2018-11-06 23:12:17 +0900

    modify signin

--- routes/user/sign_in.js
+++ routes/user/sign_in.js
@@ -9,6 +9,67 @@ const express = require('express'),
     jwt = require('../../module/jwt');
 
 //사용자 로그인
+
+// router.post('/', async (req, res, next) => {
+//     let {
+//         mail,
+//         passwd
+//     } = req.body;
+//
+//     if (check.checkNull([mail, passwd])) {
+//         res.status(400).json({
+//             message: "Null Value"
+//         })
+//     } else {
+//         let check_query = `select * from user where mail = ?;`;
+//         let check_result = await db.queryParamArr(check_query, [mail]);
+//
+//         if (!check_result) {
+//             res.status(500).json({
+//                 message: "Internal Server Error"
+//             });
+//         } else if (check_result.length === 1) {
+//             // console.log(check_result);
+//             let hashed_pw = await crypto.pbkdf2(passwd, check_result[0].salt, 100000, 32, 'sha512');
+//             if (hashed_pw.toString('base64') === check_result[0].passwd) {
+//                 let token = jwt.sign(check_result[0].user_pk, check_result[0].mail);
+//                 console.log(check_result);
+//                 const decipher2 = await crypto.decipher('aes256', secret_key.key)(check_result[0].private_key, 'base64');
+//                 console.log(decipher2.toString());
+//
+//                 let result = [{
+//                     mail: check_result[0].mail,
+//                     name: check_result[0].name,
+//                     private_key: decipher2.toString(),
+//                     birth: check_result[0].birth,
+//                     sex: check_result[0].sex,
+//                     hp: check_result[0].hp,
+//                     img: check_result[0].img,
+//                     flag: check_result[0].user_gb
+//                 }];
+//
+//                 res.status(200).send({
+//                     message: "Success To Sign In",
+//                     token: token,
+//                     data: result
+//                 });
+//             } else {
+//                 res.status(401).send({
+//                     message: "Fail To Sign In"
+//                 });
+//                 console.log("password error");
+//             }
+//         } else {
+//             res.status(401).send({
+//                 message: "Fail To Sign In"
+//             });
+//             console.log("id error");
+//         }
+//     }
+//
+// });
+
+
 router.post('/', async (req, res, next) => {
     let {
         mail,
@@ -33,13 +94,13 @@ router.post('/', async (req, res, next) => {
             if (hashed_pw.toString('base64') === check_result[0].passwd) {
                 let token = jwt.sign(check_result[0].user_pk, check_result[0].mail);
                 console.log(check_result);
-                const decipher2 = await crypto.decipher('aes256', secret_key.key)(check_result[0].private_key, 'base64');
-                console.log(decipher2.toString());
+                // const decipher2 = await crypto.decipher('aes256', secret_key.key)(check_result[0].private_key, 'base64');
+                // console.log(decipher2.toString());
 
                 let result = [{
                     mail: check_result[0].mail,
                     name: check_result[0].name,
-                    private_key: decipher2.toString(),
+                    // private_key: decipher2.toString(),
                     birth: check_result[0].birth,
                     sex: check_result[0].sex,
                     hp: check_result[0].hp,
@@ -67,7 +128,6 @@ router.post('/', async (req, res, next) => {
     }
 
 });
-
 
 
 module.exports = router;
