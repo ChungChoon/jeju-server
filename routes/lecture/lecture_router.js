e95910d48444d4e77fb1ecde3f7cdd0ce01d5db7
배지원 <bjiwon2005@naver.com>
2018-11-07 15:05:09 +0900

    default

--- routes/lecture/lecture_router.js
+++ routes/lecture/lecture_router.js
@@ -100,9 +100,9 @@ router.get('/:lectureId', async (req, res, next) => {
             }
         } else {
             let select_query = `
-            select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+            select c.idx, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
             from farmer_info a 
-            join lecture b 
+            join lecture b and lecture_idx c
             on a.user_pk = b.owner_fk
             where lecture_pk = ?
             `;
