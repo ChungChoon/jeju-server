1b6edde6d7a1f21567124d7e02228db6aed1a0fb
배지원 <bjiwon2005@naver.com>
2018-11-13 20:50:04 +0900

    add lecture filter

--- routes/home/filter_lecture.js
+++ routes/home/filter_lecture.js
@@ -25,7 +25,21 @@ router.get('/:kind', async (req, res, next) => {
     else {
         //로그인 한 사용자일 경우 구매여부, 출석률 합께 줘야함
         if (token) {
-            let select_query = `
+            let decoded = jwt.verify(token);
+            if (decoded === 10) {
+                res.status(500).json({
+                    message: "token err",
+                    expired: 1
+                });
+                return
+            }
+            if (decoded === -1) {
+                res.status(500).json({
+                    message: "token err"
+                });
+            }
+            else {
+                let select_query = `
                         select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
                         from farmer_info a 
                         join lecture b 
@@ -34,6 +48,20 @@ router.get('/:kind', async (req, res, next) => {
                         on c.lecture_fk = b.lecture_pk and c.user_fk = ?
                         order by apply desc
                         `;
+                let select_result = await db.queryParamArr(select_query, [decoded.user_idx, kind_id, decoded.user_idx]);
+
+                if (!select_result) {
+                    res.status(500).json({
+                        message: "Internal Server Error"
+                    });
+                }
+                else {
+                    res.status(200).json({
+                        message: "Success Get Filter Data",
+                        data: select_result
+                    })
+                }
+            }
         }
 
         else {
@@ -43,7 +71,20 @@ router.get('/:kind', async (req, res, next) => {
                         join lecture b 
                         on a.user_pk = b.owner_fk and b.kind  = ?
                         order by apply desc
-                        `
+                        `;
+            let select_result = await db.queryParamArr(select_query, [kind_id]);
+
+            if (!select_result) {
+                res.status(500).json({
+                    message: "Internal Server Error"
+                });
+            }
+            else {
+                res.status(200).json({
+                    message: "Success Get Filter Data",
+                    data: select_result
+                })
+            }
         }
     }
 
