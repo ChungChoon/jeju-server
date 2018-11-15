4f3b3fe7f37f57c2685a39e8228c4b93ad564d5f
배지원 <bjiwon2005@naver.com>
2018-11-15 20:39:00 +0900

    delete curriculum colum

--- routes/home/home_router.js
+++ routes/home/home_router.js
@@ -27,8 +27,7 @@ router.get('/', async (req, res, next) => {
             res.status(500).json({
                 message: "token err"
             });
-        }
-        else {
+        } else {
             let popular_query = `
                         select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
                         from farmer_info a 
@@ -75,27 +74,26 @@ router.get('/', async (req, res, next) => {
                 })
             }
         }
-    }
-    else {
+    } else {
         let popular_query = `
-                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a 
-                        join lecture b 
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.curri_count, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a
+                        join lecture b
                         on a.user_pk = b.owner_fk
                         order by apply desc
                         limit 6
                         `;
         let offline_query = `
-                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a 
-                        join lecture b 
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.curri_count, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a
+                        join lecture b
                         on a.user_pk = b.owner_fk
                         where b.kind in (3, 4, 5, 6, 7)
                         `;
         let online_query = `
-                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
-                        from farmer_info a 
-                        join lecture b 
+                        select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.curri_count, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.intro, b.limit_num, b.price, b.apply
+                        from farmer_info a
+                        join lecture b
                         on a.user_pk = b.owner_fk
                         where b.kind in (8, 9, 10, 11)
                         `;
