6a55db4543df78c16e39bd9f1c6d92627299a035
배지원 <bjiwon2005@naver.com>
2018-11-15 20:55:44 +0900

    delete curriculum colum

--- routes/lecture/lecture_router.js
+++ routes/lecture/lecture_router.js
@@ -41,7 +41,7 @@ router.get('/:lectureId', async (req, res, next) => {
                 });
             } else {
                 let select_query = `
-                    select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+                    select c.attend_cnt, (select count( * ) as buy_count from lecture_apply as a_buy where a_buy.lecture_fk = b.lecture_pk and a_buy.user_fk = ?) as check_buy, a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.curri_count, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.intro, b.limit_num, b.price, b.apply
                     from farmer_info a 
                     join lecture b 
                     on a.user_pk = b.owner_fk
@@ -52,9 +52,9 @@ router.get('/:lectureId', async (req, res, next) => {
                 let select_result = await db.queryParamArr(select_query, [decoded.user_idx, decoded.user, lecture_id]);
                 if (!select_result) {
                     res.status(500).json({
-                        message : "Internal Server Error"
+                        message: "Internal Server Error"
                     });
-                } else  {
+                } else {
                     let select_query2 = `
                     select a.user_pk, a.name, a.img, b.content, date_format(b.written_date, "%Y-%m-%d") as written_date 
                     from user a, lecture_review b
@@ -63,21 +63,32 @@ router.get('/:lectureId', async (req, res, next) => {
                     let select_result2 = await db.queryParamArr(select_query2, [lecture_id]);
                     if (!select_result) {
                         res.status(500).json({
-                            message : "Internal Server Error"
+                            message: "Internal Server Error"
                         });
-                    }
-                        else  {
+                    } else {
+                        let select_curriculum = `
+                        select a.title, b.content 
+                        from curri_title a, curri_content b 
+                        where a.lecture_fk = ? and b.lecture_fk = ? and a.title_cnt = b.content_cnt`;
+                        let curriculum_result = await db.queryParamArr(select_curriculum, [lecture_id, lecture_id]);
+                        if (!curriculum_result) {
+                            res.status(500).json({
+                                message: "Internal Server Error"
+                            });
+                        } else {
                             res.status(200).json({
-                                message : "Success Get Lecture Detail",
+                                message: "Success Get Lecture Detail",
                                 lecture_data: select_result[0],
-                                review_data: select_result2
+                                review_data: select_result2,
+                                curriculum_data: curriculum_result
                             });
                         }
+                    }
                 }
             }
         } else {
             let select_query = `
-            select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.curriculum, b.intro, b.limit_num, b.price, b.apply
+            select a.user_pk, a.name, a.img, a.user_gb, a.farm_name, a.farm_img, b.lecture_pk, b.curri_count, b.title, b.kind, date_format(b.start_date, "%Y-%m-%d") as start_date, date_format(b.end_date, "%Y-%m-%d") as end_date, date_format(b.reg_date, "%Y-%m-%d") as reg_date, b.img, b.place, b.intro, b.limit_num, b.price, b.apply
             from farmer_info a 
             join lecture b
             on a.user_pk = b.owner_fk
@@ -86,9 +97,9 @@ router.get('/:lectureId', async (req, res, next) => {
             let select_result = await db.queryParamArr(select_query, [lecture_id]);
             if (!select_result) {
                 res.status(500).json({
-                    message : "Internal Server Error"
+                    message: "Internal Server Error"
                 });
-            } else  {
+            } else {
                 let select_query2 = `
                     select a.user_pk, a.name, a.img, b.content, date_format(b.written_date, "%Y-%m-%d") as written_date 
                     from user a, lecture_review b 
@@ -97,15 +108,26 @@ router.get('/:lectureId', async (req, res, next) => {
                 let select_result2 = await db.queryParamArr(select_query2, [lecture_id]);
                 if (!select_result) {
                     res.status(500).json({
-                        message : "Internal Server Error"
+                        message: "Internal Server Error"
                     });
-                }
-                else {
-                    res.status(200).json({
-                        message : "Success Get Lecture Detail",
-                        lecture_data: select_result[0],
-                        review_data: select_result2
-                    });
+                } else {
+                    let select_curriculum = `
+                        select a.title, b.content 
+                        from curri_title a, curri_content b 
+                        where a.lecture_fk = ? and b.lecture_fk = ? and a.title_cnt = b.content_cnt`;
+                    let curriculum_result = await db.queryParamArr(select_curriculum, [lecture_id, lecture_id]);
+                    if (!curriculum_result) {
+                        res.status(500).json({
+                            message: "Internal Server Error"
+                        });
+                    } else {
+                        res.status(200).json({
+                            message: "Success Get Lecture Detail",
+                            lecture_data: select_result[0],
+                            review_data: select_result2,
+                            curriculum_data: curriculum_result
+                        });
+                    }
                 }
             }
         }
