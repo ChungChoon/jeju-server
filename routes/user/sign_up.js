25e33fb333a037944bbaa3b816c783b9d820154c
배지원 <bjiwon2005@naver.com>
2018-11-16 12:34:36 +0900

    add readme

--- routes/user/sign_up.js
+++ routes/user/sign_up.js
@@ -49,8 +49,8 @@ router.post('/', async (req, res, next) => {
                 message: "Already Exists"
             });
         } else {
-            let network_server = `http://localhost:3000`;
-            // let network_server = `http://52.78.62.162:3000`;
+            // let network_server = `http://localhost:3000`;
+            let network_server = `http://52.78.62.162:3000`;
             // let network_server = `http://37196b19.ngrok.io`;
 
             axios.post(`${network_server}`, {
